4d30c3499bc39ed7a766bfa206ab5f556f7a8a84
ampt <ampt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-22 11:16:45 +0000

    Add version 0.1
    git-svn-id: https://plugins.svn.wordpress.org/mute-screamer/trunk@265846 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/mscr/utils.php
@@ -0,0 +1,120 @@
+<?php  if ( !defined('ABSPATH') ) exit;
+
+/*
+ * Mute Screamer utils class
+ */
+class Utils {
+	/**
+	 * Load a template file
+	 *
+	 * @return	void/string
+	 */
+	public static function view( $view, $vars = array(), $return = FALSE ) {
+		$found = FALSE;
+
+		// Look in Mute Screamer views and the current Wordpress theme directories
+		for( $i = 1; $i < 3; $i++ ) {
+			$path = ($i % 2) ? MSCR_PATH . "/views/" : TEMPLATEPATH . '/';
+			$view_path = $path . $view . '.php';
+
+			// Does the file exist?
+			if( file_exists($view_path) ) {
+				$found = TRUE;
+				break;
+			}
+		}
+
+		if( $found === TRUE ) {
+			extract($vars);
+			ob_start();
+
+			include($view_path);
+
+			// Return the data if requested
+			if( $return === TRUE ) {
+				$buffer = ob_get_contents();
+				@ob_end_clean();
+				return $buffer;
+			}
+
+			$output = ob_get_contents();
+			@ob_end_clean();
+
+			echo $output;
+		} else if( defined('WP_DEBUG') && WP_DEBUG == TRUE ) {
+			trigger_error('Unable to load the requested view.', E_USER_ERROR);
+		}
+	}
+
+
+	/**
+	 * Create pagination links
+	 *
+	 * @return	string
+	 */
+	public static function pagination($current_page = 1, $total_pages = 0, $per_page = 0, $count = 0)
+	{
+		$page_links = paginate_links( array(
+			'base' => add_query_arg( 'paged', '%#%' ),
+			'format' => '',
+			'prev_text' => __('&laquo;'),
+			'next_text' => __('&raquo;'),
+			'total' => $total_pages,
+			'current' => $current_page
+		));
+
+		if( !$page_links ) {
+			return '';
+		}
+
+		$page_links_text = sprintf( '<span class="displaying-num">' . __( 'Displaying %s&#8211;%s of %s' ) . '</span>%s',
+			number_format_i18n( ( $current_page - 1 ) * $per_page + 1 ),
+			number_format_i18n( min( $current_page * $per_page, $count ) ),
+			number_format_i18n( $count ),
+			$page_links
+		);
+
+		return "<div class='tablenav-pages'>{$page_links_text}</div>";
+	}
+
+
+	/**
+	 * Get intrusions per page option
+	 *
+	 * @return	integer
+	 */
+	public static function mscr_intrusions_per_page() {
+		$per_page = (int) get_user_option('mscr_intrusions_per_page');
+
+		// Set default if user option does not exist
+		if( !$per_page ) {
+			$per_page = 20;
+		}
+
+		return $per_page;
+	}
+
+
+	/**
+	 * Get the current upload path
+	 *
+	 * @return	string
+	 */
+	public static function upload_path() {
+		$upload_path = get_option( 'upload_path' );
+
+		if ( empty($upload_path) ) {
+			$dir = WP_CONTENT_DIR . '/uploads';
+		} else {
+			$dir = $upload_path;
+			if ( 'wp-content/uploads' == $upload_path ) {
+				$dir = WP_CONTENT_DIR . '/uploads';
+			} elseif ( 0 !== strpos($dir, ABSPATH) ) {
+				// $dir is absolute, $upload_path is (maybe) relative to ABSPATH
+				$dir = path_join( ABSPATH, $dir );
+			}
+		}
+
+		return $dir;
+	}
+}
---
+++ mscr_admin.php
@@ -0,0 +1,254 @@
+<?php  if ( !defined('ABSPATH') ) exit;
+
+/**
+ * Mute Screamer admin class
+ */
+class Mscr_admin {
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		add_action( 'admin_init', array($this, 'admin_init') );
+		add_action( 'admin_menu', array($this, 'admin_menu') );
+		add_filter( 'screen_settings', array($this, 'screen_settings'), 10, 2 );
+		add_filter( 'set-screen-option', array($this, 'set_screen_option'), 10, 3 );
+	}
+
+
+	/**
+	 * Admin init
+	 *
+	 * @return	void
+	 */
+	public function admin_init() {
+		register_setting( 'mscr_options', 'mscr_options', array($this, 'options_validate') );
+		$this->do_action();
+	}
+
+
+	/**
+	 * Perform an action based on the request
+	 *
+	 * @return	void
+	 */
+	private function do_action() {
+		global $wpdb;
+
+		// Handle bulk actions
+		if ( isset( $_GET['doaction'] ) || isset( $_GET['doaction2'] ) ) {
+			check_admin_referer('mscr_action_intrusions_bulk');
+			$sendback = remove_query_arg( array('intrusions' ), wp_get_referer() );
+
+			if ( ( $_GET['action'] != -1 || $_GET['action2'] != -1 ) && ( isset($_GET['page']) && isset($_GET['intrusions']) ) ) {
+				$intrusion_ids = $_GET['intrusions'];
+				$doaction = ($_GET['action'] != -1) ? $_GET['action'] : $_GET['action2'];
+			} else {
+				wp_redirect( admin_url("index.php?page=mscr_intrusions") );
+				exit;
+			}
+
+			switch( $doaction ) {
+				case 'bulk_delete':
+					$deleted = 0;
+					foreach( (array) $intrusion_ids as $intrusion_id ) {
+						if( !current_user_can('activate_plugins') )
+							wp_die( __('You are not allowed to delete this item.') );
+
+						$sql = $wpdb->prepare( "DELETE FROM ".Mute_screamer::INTRUSIONS_TABLE." WHERE id = %d", $intrusion_id );
+						$result = $wpdb->query( $sql );
+
+						if( ! $result) {
+							wp_die( __('Error in deleting...') );
+						}
+						$deleted++;
+					}
+					$sendback = add_query_arg( 'deleted', $deleted, $sendback );
+					break;
+			}
+
+			if( isset($_GET['action']) )
+				$sendback = remove_query_arg( array('action', 'action2', 'intrusions'), $sendback );
+
+			wp_redirect($sendback);
+			exit;
+		} elseif( ! empty($_GET['_wp_http_referer']) ) {
+			wp_redirect( remove_query_arg( array('_wp_http_referer', '_wpnonce'), stripslashes($_SERVER['REQUEST_URI']) ) );
+			exit;
+		}
+	}
+
+
+	/**
+	 * Add custon screen options to a plugin page
+	 *
+	 * @param	string
+	 * @param	object
+	 * @return	string
+	 */
+	public function screen_settings( $action, $screen_object ) {
+		global $current_user;
+
+		if( $screen_object->id == 'dashboard_page_mscr_intrusions' ) {
+			$per_page = Utils::mscr_intrusions_per_page();
+
+			$data['per_page'] = $per_page;
+			$action = Utils::view('admin_intrusions_screen_options', $data, TRUE);
+		}
+
+		return $action;
+	}
+
+
+	/**
+	 * Update the current user's screen options
+	 *
+	 * @return	mixed
+	 */
+	public function set_screen_option( $flag, $option, $value ) {
+		switch( $option ) {
+			case 'mscr_intrusions_per_page':
+				$value = absint($value);
+				if( $value < 1 ) {
+					return FALSE;
+				}
+
+				return $value;
+		}
+
+		return $flag;
+	}
+
+
+	/**
+	 * Add admin menu items
+	 *
+	 * @return	void
+	 */
+	public function admin_menu() {
+		add_dashboard_page( __('Mute Screamer Intrusions'), __('Intrusions'), 'activate_plugins', 'mscr_intrusions', array($this, 'intrusions') );
+		add_options_page( __('Mute Screamer Configuration'), __('Mute Screamer'), 'activate_plugins', 'mscr_options', array($this, 'options') );
+	}
+
+
+	/**
+	 * Display PHPIDS Intrusions
+	 *
+	 * @return	void
+	 */
+	public function intrusions()
+	{
+		global $wpdb;
+
+		// Current page number, items per page
+		$per_page = Utils::mscr_intrusions_per_page();
+		$pagenum = isset( $_GET['paged'] ) ? absint( $_GET['paged'] ) : 0;
+		if ( empty($pagenum) )
+			$pagenum = 1;
+
+		// Offset, limit
+		$limit = $per_page;
+		$offset = ( $pagenum * $limit ) - $limit;
+		$offset = ( $offset < 0 ) ? 0 : $offset;
+
+		// Get results
+		$search = isset( $_GET['intrusions_search'] ) ? esc_attr($_GET['intrusions_search']) : '';
+		$search_title = '';
+		if($search) {
+			$search_title = sprintf( '<span class="subtitle">' . __('Search results for &#8220;%s&#8221;') . '</span>', $search );
+			$token = '%'.$search.'%';
+			$sql = $wpdb->prepare( "SELECT SQL_CALC_FOUND_ROWS * FROM " . Mute_screamer::INTRUSIONS_TABLE . " WHERE (name LIKE %s OR page LIKE %s OR tags LIKE %s OR ip LIKE %s OR impact LIKE %s) ORDER BY created DESC LIMIT %d, %d", $token, $token, $token, $token, $token, $offset, $limit );
+		} else {
+			$sql = $wpdb->prepare( "SELECT SQL_CALC_FOUND_ROWS * FROM " . Mute_screamer::INTRUSIONS_TABLE . " ORDER BY created DESC LIMIT %d, %d", $offset, $limit );
+		}
+
+		$intrusions = $wpdb->get_results($sql);
+		$total_intrusions = $wpdb->get_var("SELECT FOUND_ROWS();");
+
+		// Construct pagination links
+		$num_pages = ceil($total_intrusions / $per_page);
+		$pagination = Utils::pagination($pagenum, $num_pages, $per_page, $total_intrusions);
+
+		// Columns
+		$columns = array(
+			'name' => 'Name',
+			'value' => 'Value',
+			'page' => 'Page',
+			'tags' => 'Tags',
+			'ip' => 'IP',
+			'impact' => 'Impact',
+			'origin' => 'Origin',
+			'date' => 'Date'
+		);
+		$columns = apply_filters('mscr_admin_intrusions_columns', $columns);
+
+		// Was something deleted?
+		$deleted = isset($_GET['deleted']) ? (int) $_GET['deleted'] : 0;
+
+		$data['message'] = FALSE;
+		$data['intrusions'] = $intrusions;
+		$data['style'] = '';
+		$data['columns'] = $columns;
+		$data['page'] = $_GET['page'];
+		$data['pagination'] = $pagination;
+		$data['intrusions_search'] = $search;
+		$data['search_title'] = $search_title;
+
+		if( $deleted )
+			$data['message'] = sprintf( _n( 'Item permanently deleted.', '%s items permanently deleted.', $deleted ), number_format_i18n( $deleted ) );
+
+		Utils::view('admin_intrusions', $data);
+	}
+
+
+	/**
+	 * Validate options
+	 *
+	 * @return	array
+	 */
+	public function options_validate( $input ) {
+		$options = get_option( 'mscr_options' );
+
+		foreach( array( 'email', 'email_threshold', 'exception_fields', 'html_fields', 'json_fields' ) as $key ) {
+			if( isset($input[$key]) ) {
+				$options[$key] = $input[$key];
+			}
+
+			switch($key) {
+				case 'email':
+					if( !is_email($options[$key]) ) {
+						$options[$key] = get_option('admin_email');
+					}
+					break;
+
+				case 'email_threshold':
+					$options[$key] = absint($options[$key]);
+					break;
+
+				case 'exception_fields':
+				case 'html_fields':
+				case 'json_fields':
+					$options[$key] = str_replace( array( "\r\n", "\n", "\r" ), "\n", $options[$key] );
+					$options[$key] = explode( "\n", $options[$key] );
+			}
+		}
+
+		$options['email_notifications'] = isset($input['email_notifications']) ? 1 : 0;
+		return $options;
+	}
+
+
+	/**
+	 * Display options page
+	 *
+	 * @return	void
+	 */
+	public function options()
+	{
+		$options = get_option( 'mscr_options' );
+		$options['exception_fields'] = implode("\r\n", $options['exception_fields']);
+		$options['html_fields'] = implode("\r\n", $options['html_fields']);
+		$options['json_fields'] = implode("\r\n", $options['json_fields']);
+
+		Utils::view('admin_options', $options);
+	}
+}
